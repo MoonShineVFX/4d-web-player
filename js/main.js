7b7e32f50b251b5e847205ed8acd590612c21cd3
eli.hung <mrhchief@gmail.com>
2022-04-12 18:41:05 +0800

    refactor: Put excluded files together

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // make urls
 let urls = [];
-const url = '/gltf_mini_drc/'
+const url = '/resource/gltf_mini_drc/'
 const frameCount = 1038;
 for (let i = 0; i < frameCount; i++) {
   urls[i] = url + String(i).padStart(4, '0') + '.glb';
